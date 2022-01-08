053586a3286ca12124c1d4b923bdc900676d4848
Mario Shebib <38814197+MarioShebib@users.noreply.github.com>
2022-01-08 12:32:19 -0500

    [CODE] Completed working gauges for overview page
    Gauges are working, all that is left is to link them to the data in the graphs and coming in through thingspeak.

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -1,23 +1,28 @@
-import Gauge from "../components/Gauge/Gauge";
+import { RadialGauge, RadialGaugeSeries } from "reaviz";
 
-const SpeedDial = new Gauge(
-  "Speed",
-  420, 
-  69, 
-  100
-  );
+const data = [
+  { key: "Speed", data: 40 },
+  { key: "EMG 1", data: 20 },
+  { key: "EMG 2", data: 14 },
+  { key: "EMG 3", data: 19 },
+  { key: "EMG 4", data: 30 },
+  { key: "EMG 5", data: 47 },
+  { key: "EMG 6", data: 75 },
+  { key: "Pressure Plate Left", data: 25 },
+  { key: "Pressure Plate Right", data: 25 },
+];
 const Overview = () => {
-
-  return( 
-  <div>hi from overview page
-    <div>    
-    <SpeedDial
-    />
+  return (
+    <div>
+      hi from overview page
+      <RadialGauge
+        width={800}
+        height={450}
+        data={data}
+        series={<RadialGaugeSeries minGaugeWidth={150} />}
+      />
     </div>
-
-  </div>
-  )
+  );
 };
-console.log("The speed dial: ", SpeedDial);
 
 export default Overview;
