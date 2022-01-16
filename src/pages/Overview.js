1c39224546eefcc751f45779b27ca4fbe137349d
Mario Shebib <38814197+MarioShebib@users.noreply.github.com>
2022-01-16 11:03:11 -0500

    Working all gauges

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -5,6 +5,41 @@ import DropdownDrills from "../components/DropdownDrills/DropdownDrills";
 import HockeySwitch from "../components/HockeySwitch/HockeySwitch";
 import DropdownNames from "../components/DropdownNames/DropdownNames";
 import TypeSwitch from "../components/TypeSwitch/TypeSwitch";
+import { RadialGauge, RadialGaugeSeries } from "reaviz";
+
+const dataSource = {
+  //dataSource 0->9 correspond to Speed ->Pressure Plate Right Need more dataSources
+  speedData: dataSourceSet("EmgData"),
+  emg1Data: dataSourceSet("EmgData"),
+  emg2Data: dataSourceSet("EmgData"),
+  emg3Data: dataSourceSet("EmgData"),
+  emg4Data: dataSourceSet("EmgData"),
+  emg5Data: dataSourceSet("EmgData"),
+  emg6Data: dataSourceSet("EmgData"),
+  pressurePlateLeft: dataSourceSet("EmgData"),
+  pressurePlateRight: dataSourceSet("EmgData"),
+};
+/**
+ *
+ * @param {*} source
+ * @returns a number based off of the current value of the information source
+ */
+function dataSourceSet(source) {
+  return parseFloat(JSON.parse(localStorage.getItem(source)));
+}
+
+const data = [
+  { key: "Speed", data: dataSource.speedData },
+  { key: "EMG 1", data: dataSource.emg1Data },
+  { key: "EMG 2", data: dataSource.emg2Data },
+  { key: "EMG 3", data: dataSource.emg3Data },
+  { key: "EMG 4", data: dataSource.emg4Data },
+  { key: "EMG 5", data: dataSource.emg5Data },
+  { key: "EMG 6", data: dataSource.emg6Data },
+  { key: "Pressure Plate Left", data: dataSource.pressurePlateLeft },
+  { key: "Pressure Plate Right", data: dataSource.pressurePlateRight },
+];
+
 const Overview = () => {
   return (
     <div>
@@ -21,6 +56,12 @@ const Overview = () => {
         <DropdownDrills />
         <RecordButton />
         <ClearButton />
+        <RadialGauge
+          width={800}
+          height={450}
+          data={data}
+          series={<RadialGaugeSeries minGaugeWidth={150} />}
+        />
       </div>
     </div>
   );
